c5d640b1045046616038c19b923bf462b119350b
Asmit356 <asmittripathi0911@gmail.com>
2025-11-22 14:18:28 +0530

    Implement signaling server with Socket.IO

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("join-room", (room) => {
+        socket.join(room);
+        socket.to(room).emit("user-joined", socket.id);
+    });
+
+    socket.on("signal", (data) => {
+        io.to(data.to).emit("signal", {
+            from: data.from,
+            signal: data.signal
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+        socket.broadcast.emit("user-left", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+    console.log("🚀 Signaling server running on port", PORT);
+});
